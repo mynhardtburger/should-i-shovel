b4f39c878d08d516fffc29b2708f4d4b58964e05
mynhardtburger <mynhardt@gmail.com>
2022-12-24 21:38:18 -0500

    basic client charts and input

--- api/main.py
+++ api/main.py
@@ -1,18 +1,33 @@
-from fastapi import FastAPI
+import logging
+from urllib.parse import unquote
+
+import geocoder
 import predictions
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 # Define connection details
 pg_connection_dict = {
-    'dbname': "mydb",
-    'user': "myn",
-    'password': r"2)2K9zJCKZv7pLUd",
-    'port': "5432",
-    'host': "terraform-20221222010822007100000002.c2x7llrlmsr3.us-east-2.rds.amazonaws.com"
+    "dbname": "mydb",
+    "user": "myn",
+    "password": r"2)2K9zJCKZv7pLUd",
+    "port": "5432",
+    "host": "terraform-20221222010822007100000002.c2x7llrlmsr3.us-east-2.rds.amazonaws.com",
 }
 
 
-
 app = FastAPI()
+
+origins = ["http://localhost:1234"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 def read_root():
@@ -21,4 +36,19 @@ def read_root():
 
 @app.get("/forecast/")
 def get_forecast(latitude: float, longitude: float):
-    return predictions.get_nearest_predictions_as_df(pg_connection_dict, latitude, longitude)
+    return predictions.get_nearest_predictions_as_df(
+        pg_connection_dict, latitude, longitude
+    )
+
+
+@app.get("/address/")
+def get_address_coordinates(address: str) -> tuple[float, float]:
+    """Queries geocoding API to get coordinates from address. Returns tuple of nulls if no single result was found"""
+    address_unquoted = unquote(address)
+    g = geocoder.google(address_unquoted)
+    if g.error == "ZERO_RESULTS":
+        logging.warn(f"{g.error}: {address_unquoted}")
+    if g.error == "REQUEST_DENIED":
+        logging.critical(f"API error: {g.error}")
+
+    return g.lat, g.lng
--- client/src/shouldishovel.js
+++ client/src/shouldishovel.js
@@ -1,13 +1,111 @@
-function logSubmit() {
-    window.alert("sometext");
+import Chart from 'chart.js/auto';
+import 'chartjs-adapter-moment';
+
+var chart = null;
+
+function get_forecast(lat, lng) {
+  // lat = document.getElementById('f_lat').value;
+  // lon = document.getElementById('f_lon').value;
+
+  fetch(`http://127.0.0.1:8000/forecast/?latitude=${lat}&longitude=${lng}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+
+      const chartData = {
+        labels: Object.values(data['forecast_time']),
+        datasets: [
+          {
+            label: data['long_name'][0],
+            data: Object.values(data['value']),
+            borderColor: 'rgb(255, 99, 132)',
+            fill: false,
+            cubicInterpolationMode: 'monotone',
+            tension: 0.4,
+          },
+        ],
+      };
+
+      if (chart === null) {
+        chart = draw_chart(chartData);
+      } else {
+        chart.data.labels = chartData.labels;
+        chart.data.datasets = chartData.datasets;
+        chart.update();
+      }
+    })
+    .catch(console.error);
 }
 
-function request_forecast(lat, lon) {
-    lat = document.getElementById("f_lat").value
-    lon = document.getElementById("f_lon").value
-    res = fetch(`http://127.0.0.1:8000/forecast/?latitude=${lat}&longitude=${lon}`)
-    console.log(res)
+function draw_chart(chartdata) {
+  return new Chart(document.getElementById('charts'), {
+    type: 'line',
+    data: chartdata,
+    options: {
+      responsive: true,
+      plugins: {
+        title: {
+          display: true,
+          text: '48 hour forecast',
+        },
+        legend: {
+          display: false,
+        },
+      },
+      interaction: {
+        intersect: false,
+      },
+      scales: {
+        x: {
+          display: true,
+          title: {
+            display: true,
+          },
+          type: 'timeseries',
+          time: {
+            displayFormats: {
+              hour: 'DD MMM hh:mm A',
+            },
+            unit: 'hour',
+            unitStepSize: 1,
+          },
+        },
+        y: {
+          display: true,
+          title: {
+            display: true,
+            text: 'Meters',
+          },
+          suggestedMin: 0,
+          suggestedMax: 1,
+        },
+      },
+    },
+  });
 }
 
-const form = document.getElementById('lat_lon_form');
-form.addEventListener('submit', request_forecast());
+function get_forecast_for_address() {
+  address = document.getElementById('address').value;
+
+  fetch(`http://127.0.0.1:8000/address/?address=${address}`)
+    .then((response) => response.json())
+    .then((coords) => {
+      console.log(coords);
+      if (coords[0] === null) {
+        console.log('No coordinates found');
+      } else {
+        get_forecast(coords[0], coords[1]);
+      }
+    })
+    .catch(console.error);
+}
+
+const button = document.getElementById('getData');
+const button_address = document.getElementById('getData_address');
+button.addEventListener('click', () =>
+  get_forecast(
+    document.getElementById('f_lat').value,
+    document.getElementById('f_lng').value
+  )
+);
+button_address.addEventListener('click', get_forecast_for_address);
