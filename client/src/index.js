3e770162f44092f9d1777d25e2b30cbb9a471292
mynhardtburger <mynhardt@gmail.com>
2023-01-17 22:43:46 -0500

    google maps

---
+++ client/src/index.js
@@ -0,0 +1,118 @@
+import { get_forecast } from './shouldishovel';
+import './style.css';
+
+function printMe() {
+  console.log('I get called from print.js!');
+}
+
+function component() {
+  const element = document.createElement('div');
+  const btn = document.createElement('button');
+
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+
+  return element;
+}
+
+// document.body.appendChild(component());
+
+let map, infoWindow;
+let markers = [];
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: 49.886061, lng: -97.126819 },
+    zoom: 5,
+  });
+  infoWindow = new google.maps.InfoWindow();
+
+  const locationButton = document.createElement('button');
+
+  locationButton.textContent = 'Pan to Current Location';
+  locationButton.classList.add('custom-map-control-button');
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+  locationButton.addEventListener('click', () => {
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+
+          infoWindow.setPosition(pos);
+          infoWindow.setContent('Location found.');
+          infoWindow.open(map);
+          map.setCenter(pos);
+        },
+        () => {
+          handleLocationError(true, infoWindow, map.getCenter());
+        }
+      );
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+  });
+
+  map.addListener('click', (mapsMouseEvent) => {
+    // map.setZoom(8);
+    deleteMarkers();
+    addMarker(mapsMouseEvent.latLng);
+    map.panTo(mapsMouseEvent.latLng);
+    console.log(mapsMouseEvent.latLng.toString());
+    get_forecast(mapsMouseEvent.latLng.lat(), mapsMouseEvent.latLng.lng());
+  });
+}
+
+function addMarker(position) {
+  const marker = new google.maps.Marker({
+    position,
+    map,
+  });
+
+  markers.push(marker);
+}
+
+function setMapOnAll(map) {
+  for (let i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
+  }
+}
+
+function hideMarkers() {
+  setMapOnAll(null);
+}
+
+function deleteMarkers() {
+  hideMarkers();
+  markers = [];
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? 'Error: The Geolocation service failed.'
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
+}
+
+window.initMap = initMap;
+
+// const button = document.getElementById('getData');
+// const button_address = document.getElementById('getData_address');
+// button.addEventListener('click', () =>
+//   get_forecast(
+//     document.getElementById('f_lat').value,
+//     document.getElementById('f_lng').value
+//   )
+// );
+// button_address.addEventListener('click', get_forecast_for_address);
--- client/src/shouldishovel.js
+++ client/src/shouldishovel.js
@@ -1,43 +1,48 @@
 import Chart from 'chart.js/auto';
 import 'chartjs-adapter-moment';
+import _ from 'lodash';
 
 var chart = null;
 
-function get_forecast(lat, lng) {
+export function get_forecast(lat, lng) {
   // lat = document.getElementById('f_lat').value;
   // lon = document.getElementById('f_lon').value;
 
-  fetch(`http://127.0.0.1:8000/forecast/?latitude=${lat}&longitude=${lng}`)
+  fetch(
+    `http://127.0.0.1:8000/forecast/coordinates?latitude=${lat}&longitude=${lng}`
+  )
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
 
-      const chartData = {
-        labels: Object.values(data['forecast_time']),
-        datasets: [
-          {
-            label: data['long_name'][0],
-            data: Object.values(data['value']),
-            borderColor: 'rgb(255, 99, 132)',
-            fill: false,
-            cubicInterpolationMode: 'monotone',
-            tension: 0.4,
-          },
-        ],
-      };
+      for (let forecast_var of data) {
+        const chartData = {
+          labels: Object.values(forecast_var['forecast_timestamp']),
+          datasets: [
+            {
+              label: forecast_var['variable'][0],
+              data: Object.values(forecast_var['value']),
+              borderColor: 'rgb(255, 99, 132)',
+              fill: false,
+              cubicInterpolationMode: 'monotone',
+              tension: 0.4,
+            },
+          ],
+        };
 
-      if (chart === null) {
-        chart = draw_chart(chartData);
-      } else {
-        chart.data.labels = chartData.labels;
-        chart.data.datasets = chartData.datasets;
-        chart.update();
+        if (chart === null) {
+          chart = draw_chart(chartData);
+        } else {
+          // chart.data.labels = chartData.labels;
+          // chart.data.datasets = chartData.datasets;
+          // chart.update();
+        }
       }
     })
     .catch(console.error);
 }
 
-function draw_chart(chartdata) {
+export function draw_chart(chartdata) {
   return new Chart(document.getElementById('charts'), {
     type: 'line',
     data: chartdata,
@@ -84,7 +89,7 @@ function draw_chart(chartdata) {
   });
 }
 
-function get_forecast_for_address() {
+export function get_forecast_for_address() {
   address = document.getElementById('address').value;
 
   fetch(`http://127.0.0.1:8000/address/?address=${address}`)
@@ -99,13 +104,3 @@ function get_forecast_for_address() {
     })
     .catch(console.error);
 }
-
-const button = document.getElementById('getData');
-const button_address = document.getElementById('getData_address');
-button.addEventListener('click', () =>
-  get_forecast(
-    document.getElementById('f_lat').value,
-    document.getElementById('f_lng').value
-  )
-);
-button_address.addEventListener('click', get_forecast_for_address);
