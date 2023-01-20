030d8ef57dd901105ac216e63e3653f12859d796
mynhardtburger <mynhardt@gmail.com>
2023-01-19 22:06:38 -0500

    variable_details dimension table

--- containers/postgis/99_setup.sh
+++ containers/postgis/99_setup.sh
@@ -15,6 +15,18 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     );
     CREATE INDEX ON "public"."predictions" USING gist (st_convexhull("raster"));
 
+    CREATE TABLE public.variable_definitions (
+        model text NOT NULL,
+        variable text NOT NULL,
+        description text NULL,
+        unit text NOT NULL,
+        notes text NULL,
+        CONSTRAINT variable_definitions_pk PRIMARY KEY (model,variable)
+    );
+
+    COPY variable_definitions("model", "variable", "description", "unit", "notes")
+    FROM '/var/lib/postgresql/variables.csv' (delimiter ',', header TRUE);
+
     CREATE TABLE public.variables (
         filename text NOT NULL,
         forecast_base_string text NOT NULL,
@@ -22,7 +34,6 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
         forecast_start_timestamp timestamptz NOT NULL,
         source text NOT NULL,
         model text NOT NULL,
-        domain text NOT NULL,
         variable text NOT NULL,
         leveltype text NOT NULL,
         level text NOT NULL,
@@ -31,4 +42,5 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     );
 
     ALTER TABLE public.predictions ADD CONSTRAINT predictions_fk FOREIGN KEY (filename) REFERENCES public.variables(filename) ON DELETE CASCADE ON UPDATE CASCADE;
+    ALTER TABLE public.variables ADD CONSTRAINT variables_fk FOREIGN KEY (model,variable) REFERENCES public.variable_definitions(model,variable);
 EOSQL
